f1d2170a7aaff0f13d57ff1128540658d6589eb5
RayCL2 <bast.bascunan@duocuc.cl>
2025-05-16 22:22:26 -0400

    #Se borro lo que provocaba el postmorten

--- backend/apps/views.py
+++ backend/apps/views.py
@@ -91,9 +91,6 @@ class ProductoListView(LoginRequiredMixin, ListView):
     template_name = "admin/productos_list.html"
     context_object_name = "productos"
 
-class ProductosListView(ListView):
-    model = Producto
-    template_name = "templates/catalogue.html"
 
 class productoCreateView(CreateView):
     model = Producto
