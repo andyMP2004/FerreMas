5543d510a0c38e229b3391f6ddac699f0f89b1f1
RayCL2 <bast.bascunan@duocuc.cl>
2025-06-06 10:19:32 -0400

    urls

--- backend/apps/urls.py
+++ backend/apps/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
+    CategoriaViewSet,
     OrderViewSet,
     OrderItemViewSet,
+    UserViewSet,
+    productoViewSet,
 )
 from .views import (
     productoCreateView,
@@ -13,7 +16,9 @@ from .views import (
 router = DefaultRouter()
 router.register(r'Order', OrderViewSet, basename='order')
 router.register(r'OrdeItem', OrderItemViewSet, basename='orderitem')
-
+router.register(r'producto', productoViewSet, basename='producto')
+router.register(r'categoria', CategoriaViewSet, basename='categoria')
+router.register(r'user', UserViewSet, basename='user')
 
 urlpatterns = [
     *router.urls,
